5165c07cc819928aaa1ffd249b92cec95ba5d968
data-miner00 <data-miner00@users.noreply.github.com>
2024-02-08 03:24:26 +0800

    feat: Use webserver instead

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 import { a } from "./interval.js";
-const value = 1;
+import http from "http";
 
-console.log(value + a);
+const port = 2999 + a;
+
+const server = http.createServer(function (_req: any, res: any) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.write("hello world");
+  res.end();
+});
+
+server.listen(port, function (error: unknown) {
+  if (error) {
+    console.error("Something went wrong: ", error);
+  } else {
+    console.log("Server listening on port " + port);
+  }
+});
